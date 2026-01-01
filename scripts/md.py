6695a40df7fbb14264faf37610bd7a9f22eddcb9
Nam Dao <namdao2404@berkeley.edu>
2026-01-01 13:11:24 +0700

    Update md.py

--- scripts/md.py
+++ scripts/md.py
@@ -68,9 +68,9 @@ dyn.run(equilibration_steps)
 ##run##
 md_logger = MDLogger(dyn, atoms, logfile=logfile, 
                      header=True, stress=False, mode='w')
-dyn.attach(md_logger, interval=100) #md log check
+dyn.attach(md_logger, interval=1) #md log check
 traj = Trajectory(trajectory_file, 'w', atoms)
-dyn.attach(traj.write, interval=100) ## will write .traj file
+dyn.attach(traj.write, interval=1) ## will write .traj file
 print("Starting equilibration (NVT) ...")
 dyn.run(nsteps)
 print("complete.")
